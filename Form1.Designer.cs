a3590964bd14eca1a5bed13bdf60ba45a6644280
ryokochang <ryokochang@hotmail.com>
2017-11-29 13:45:04 -0200

    1.0
    Primeira versão

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace LidarSync
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_load_folder = new System.Windows.Forms.Button();
+            this.button_load_apx = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button_cancel = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.button_process = new System.Windows.Forms.Button();
+            this.label_apx = new System.Windows.Forms.Label();
+            this.label_folder = new System.Windows.Forms.Label();
+            this.textBox_path_apx = new System.Windows.Forms.TextBox();
+            this.textBox_path_lidar = new System.Windows.Forms.TextBox();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_load_folder
+            // 
+            this.button_load_folder.Location = new System.Drawing.Point(350, 52);
+            this.button_load_folder.Name = "button_load_folder";
+            this.button_load_folder.Size = new System.Drawing.Size(75, 23);
+            this.button_load_folder.TabIndex = 0;
+            this.button_load_folder.Text = "Select";
+            this.button_load_folder.UseVisualStyleBackColor = true;
+            this.button_load_folder.Click += new System.EventHandler(this.button_load_folder_Click);
+            // 
+            // button_load_apx
+            // 
+            this.button_load_apx.Location = new System.Drawing.Point(350, 126);
+            this.button_load_apx.Name = "button_load_apx";
+            this.button_load_apx.Size = new System.Drawing.Size(75, 23);
+            this.button_load_apx.TabIndex = 1;
+            this.button_load_apx.Text = "Select";
+            this.button_load_apx.UseVisualStyleBackColor = true;
+            this.button_load_apx.Click += new System.EventHandler(this.button_load_apx_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button_cancel);
+            this.panel1.Controls.Add(this.progressBar);
+            this.panel1.Controls.Add(this.button_process);
+            this.panel1.Controls.Add(this.label_apx);
+            this.panel1.Controls.Add(this.label_folder);
+            this.panel1.Controls.Add(this.textBox_path_apx);
+            this.panel1.Controls.Add(this.textBox_path_lidar);
+            this.panel1.Controls.Add(this.button_load_folder);
+            this.panel1.Controls.Add(this.button_load_apx);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(464, 261);
+            this.panel1.TabIndex = 2;
+            // 
+            // button_cancel
+            // 
+            this.button_cancel.Location = new System.Drawing.Point(272, 215);
+            this.button_cancel.Name = "button_cancel";
+            this.button_cancel.Size = new System.Drawing.Size(94, 34);
+            this.button_cancel.TabIndex = 8;
+            this.button_cancel.Text = "Cancel";
+            this.button_cancel.UseVisualStyleBackColor = true;
+            this.button_cancel.Click += new System.EventHandler(this.button_cancel_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 178);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(440, 25);
+            this.progressBar.TabIndex = 7;
+            // 
+            // button_process
+            // 
+            this.button_process.Location = new System.Drawing.Point(99, 215);
+            this.button_process.Name = "button_process";
+            this.button_process.Size = new System.Drawing.Size(94, 34);
+            this.button_process.TabIndex = 6;
+            this.button_process.Text = "Start";
+            this.button_process.UseVisualStyleBackColor = true;
+            this.button_process.Click += new System.EventHandler(this.button_process_Click);
+            // 
+            // label_apx
+            // 
+            this.label_apx.AutoSize = true;
+            this.label_apx.Location = new System.Drawing.Point(29, 101);
+            this.label_apx.Name = "label_apx";
+            this.label_apx.Size = new System.Drawing.Size(53, 13);
+            this.label_apx.TabIndex = 5;
+            this.label_apx.Text = "Select file";
+            // 
+            // label_folder
+            // 
+            this.label_folder.AutoSize = true;
+            this.label_folder.Location = new System.Drawing.Point(29, 29);
+            this.label_folder.Name = "label_folder";
+            this.label_folder.Size = new System.Drawing.Size(66, 13);
+            this.label_folder.TabIndex = 4;
+            this.label_folder.Text = "Select folder";
+            // 
+            // textBox_path_apx
+            // 
+            this.textBox_path_apx.Location = new System.Drawing.Point(32, 128);
+            this.textBox_path_apx.Name = "textBox_path_apx";
+            this.textBox_path_apx.Size = new System.Drawing.Size(312, 20);
+            this.textBox_path_apx.TabIndex = 3;
+            // 
+            // textBox_path_lidar
+            // 
+            this.textBox_path_lidar.Location = new System.Drawing.Point(32, 54);
+            this.textBox_path_lidar.Name = "textBox_path_lidar";
+            this.textBox_path_lidar.Size = new System.Drawing.Size(312, 20);
+            this.textBox_path_lidar.TabIndex = 2;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 261);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Lidar Data Sync";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_load_folder;
+        private System.Windows.Forms.Button button_load_apx;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label_apx;
+        private System.Windows.Forms.Label label_folder;
+        private System.Windows.Forms.TextBox textBox_path_apx;
+        private System.Windows.Forms.TextBox textBox_path_lidar;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button button_process;
+        private System.Windows.Forms.Button button_cancel;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LidarSync
+{
+    public partial class Form1 : Form
+    {
+        string[] frames;
+        string path_apx, all_frames;
+        int total_frames;
+ 
+        public Form1()
+        {
+            InitializeComponent();
+  
+        }
+
+        private void button_load_folder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog diag = new FolderBrowserDialog();
+            diag.ShowDialog();
+            textBox_path_lidar.Text = diag.SelectedPath;
+            if (diag.SelectedPath != "")
+            {
+                this.frames = Directory.GetFiles(Convert.ToString(textBox_path_lidar.Text), "*.csv", System.IO.SearchOption.TopDirectoryOnly);
+            }
+            else
+                return;
+        }
+
+        private void button_load_apx_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog diag = new OpenFileDialog();
+            diag.Filter = "Txt file| *.txt";
+            diag.ShowDialog();
+            if(diag.CheckFileExists)
+            {
+                textBox_path_apx.Text = diag.FileName;
+                this.path_apx = Convert.ToString(textBox_path_apx.Text);
+            }
+        }
+
+        private void button_process_Click(object sender, EventArgs e)
+        {
+            if (textBox_path_apx.Text == "" || textBox_path_lidar.Text == "")
+            {
+                MessageBox.Show("Arquivos não selecionados!");
+                return;
+            }
+            SaveFileDialog saved = new SaveFileDialog();
+            saved.Filter = "Comma-separated values|*.Csv";
+            saved.DefaultExt = ".csv";
+            saved.ShowDialog();
+
+            if(saved.FileName ==  "")
+            {
+                return;
+            }
+
+            this.all_frames = saved.FileName;
+
+            backgroundWorker.RunWorkerAsync();
+        }
+        private void button_cancel_Click(object sender, EventArgs e)
+        {
+            progressBar.Value = 0;
+            backgroundWorker.CancelAsync();
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            this.total_frames = frames.Length;
+            int once=0;
+            string newLine;
+
+            var fileApx = File.OpenRead(path_apx);
+            var fileSave = File.OpenWrite(all_frames);
+            var readerApx = new StreamReader(fileApx);
+            var writer = new StreamWriter(fileSave);
+
+            //discart firt line of apx file
+            var lineApx = readerApx.ReadLine();
+            var valuesApx = lineApx.Split(',');
+            // ---------------------------- //
+
+            for (int i = 0; i < total_frames; i++)
+            {
+                if (backgroundWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                }
+                else
+                {
+                    var fileFrames = File.ReadAllLines(frames[i]);
+                    var lastLineFrame = fileFrames[fileFrames.Length - 1];
+                    var valuesFrame = lastLineFrame.Split(',');
+
+                    lineApx = readerApx.ReadLine();
+                    valuesApx = lineApx.Split(',');
+                    if (once == 0)
+                    {
+                        //do the first line of file
+                        newLine = "X,Y,Z,GPS TIME,EASTING,NORTHING,ELLIPSOID HEIGHT,PITCH,ROLL,HEADING";
+                        writer.WriteLine(newLine);
+                        once++;
+                    }
+
+                    while (Convert.ToDouble(valuesApx[0]) < (Convert.ToDouble(valuesFrame[10])/10))
+                    {
+                        lineApx = readerApx.ReadLine();
+                        valuesApx = lineApx.Split(',');
+                    }
+                    for (int j = 1; j < fileFrames.Length; j++)
+                    {
+                        var temp = fileFrames[j].Split(',');
+                        newLine = temp[3] + ',' + temp[4] + ',' + temp[5] + ',' + 
+                                valuesApx[0] + ',' + valuesApx[2] + ',' + valuesApx[3] + ',' + 
+                                valuesApx[4] + ',' + valuesApx[5] + ',' + valuesApx[6] + ',' + valuesApx[7];
+
+                        writer.WriteLine(newLine);
+                    }                   
+                    backgroundWorker.ReportProgress(i);
+                }
+            }
+            readerApx.Close();
+            writer.Close();             
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Maximum = total_frames-1;
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {          
+            if (e.Cancelled)
+            {                
+                MessageBox.Show("Cancelled");
+            }
+            else
+            {
+                MessageBox.Show("Complete");
+                progressBar.Value = 0;
+            }            
+        }
+    }
+}
